3ea01a2fdf50347cd0389ca70f562f1808d6b188
Kaempe_Laptop <Nikolajkaempe@hotmail.com>
2017-05-11 11:07:50 +0200

    added mehod for getting a currency from a ipaddress

--- src/main/java/controllers/RegistrationController.java
+++ src/main/java/controllers/RegistrationController.java
@@ -7,6 +7,7 @@ import soap_services.geoIP.webservicex.GeoIPService;
 import soap_services.geoIP.webservicex.GeoIPServiceSoap;
 
 import static jsonUtil.JsonUtil.json;
+import static spark.Spark.after;
 import static spark.Spark.get;
 
 /**
@@ -47,6 +48,42 @@ public class RegistrationController
             res.status(400);
             return new String ("Unable to get a respons from GeoIpService");
         }, json());
+
+        get("/currency", (req, res) ->
+        {
+            GeoIP ipResult = null;
+            String countries;
+            String[] currency;
+            String[] result;
+            try
+            {
+                GeoIPService ipService = new GeoIPService();
+                GeoIPServiceSoap ipInterface = ipService.getGeoIPServiceSoap();
+                Country countryService = new Country();
+                CountrySoap countryInterface = countryService.getCountrySoap();
+
+                String ip = req.ip();
+                System.out.println(ip);
+                ipResult = ipInterface.getGeoIP(ip);
+                System.out.println(ipResult.getCountryName());
+                countries = countryInterface.getCurrencyByCountry(ipResult.getCountryName());
+                currency = countries.split("<CurrencyCode>",3);
+                result = currency[1].split("</CurrencyCode>",3);
+                if (result[0].length() > 0) return result[0];
+            }catch (Exception e)
+            {
+                res.status(400);
+                return new String ("Unable to access GeoIpService");
+            }
+
+            res.status(400);
+            return new String ("Unable to get a respons from GeoIpService");
+        }, json());
+
+        after((req, res) -> {
+            res.type("application/json");
+            res.header("responseServer","Server: 2");
+        });
     }
 
 }
